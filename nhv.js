7c7d327726040edc80e68b8a0f036e134a4a7e93
MGPuneeth <37puneethmg472005@gmail.com>
2024-11-18 23:19:41 +0530

    Created Hero Landing page for NH-Verse

---
+++ nhv.js
@@ -0,0 +1,16 @@
+const button1=document.getElementById('alert1');
+button1.addEventListener('click',function(){
+    alert('Are you sure to leave this page?');
+});
+const button2=document.getElementById('alert2');
+button2.addEventListener('click',function(){
+    alert('Are you sure to leave this page?');
+});
+const button3=document.getElementById('alert3');
+button3.addEventListener('click',function(){
+    alert('Are you sure to leave this page?');
+});
+const button4 = document.getElementById('alert4');
+button4.addEventListener('click', function() {
+    alert('Are you sure to leave this page?');
+});
